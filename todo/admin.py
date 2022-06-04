00ede7af7ef1512f8a53ab02e0a0d462dc28e3bc
Lily Perera <lilycapetillo86@gmail.com>
2022-06-04 18:10:30 -0400

    [ToDo] Creating to the AdminModel

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ToDo  # new
 
 # Register your models here.
+admin.register(ToDo)  # new
+class ToDoAdmin(admin.ModelAdmin):  # new
+    list_display = ['title', 'complete', 'created_at']  # new
+    list_filter = ['complete', 'created_at']  # new
+    readonly_fields = ['created_at']  # new
