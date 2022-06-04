a727bd911aad1143794137a6b04b441503aa42df
Lily Perera <lilycapetillo86@gmail.com>
2022-06-04 13:41:46 -0400

    [ToDo] Create ToDo Model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+class ToDo(models.Model): #
+    title = models.CharField(max_length=80)
+    complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "ToDo"
+        verbose_name_plural = "ToDos"
+
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("todo:detail", kwargs={"pk": self.pk})
+
+    def is_complete(self):
+        return self.complete
+
+    def get_mark_complete_url(self):
+        return reverse("todo:mark_complete", kwargs={"pk": self.pk})
+
+
